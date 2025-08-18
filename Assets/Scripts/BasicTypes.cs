c9e3e7609ab6c25365b71adc36c40823dd51c8fa
sx <xsuibo@gmail.com>
2025-08-18 15:41:33 +0800

    [Setup] Fix UnityRunner updating loop

--- Assets/Scripts/BasicTypes.cs
+++ Assets/Scripts/BasicTypes.cs
@@ -5,25 +5,26 @@ namespace Trajectory_Planner_Augmentus
     // Point in N-dimensional space 
     public class Point
     {
-        public List<double> m_dims = new List<double>();
+        public List<double> dims = new List<double>();
 
         public double[] convertToArray()
         {
-            return m_dims.ToArray();
+            return dims.ToArray();
         }
     }
     
     public class Path
     {
-        public List<Point> m_points = new List<Point>();
+        public List<Point> points = new List<Point>();
     }
 
     public class Trajectory
     {
-        public List<Point> m_positions = new List<Point>();
-        public List<Point> m_velocities = new List<Point>();
-        public List<Point> m_accelerations = new List<Point>();
-        public double time_interval = new double();
+        public List<Point> positions = new List<Point>();
+        public List<Point> velocities = new List<Point>();
+        public List<Point> accelerations = new List<Point>();
+        public int Count = 0;
+        public double time_interval = 1;
     }
 
 } // namespace Trajectory_Planner_Augmentus
--- Assets/Scripts/UnityRunner.cs
+++ Assets/Scripts/UnityRunner.cs
@@ -6,6 +6,10 @@ namespace Trajectory_Planner_Augmentus
     {
         private PathPlanner path_planner = new PathPlanner();
         private TrajectoryParameterizer trajectory_parameterizer = new TrajectoryParameterizer();
+        private Path m_path;
+        private Trajectory m_traj;
+        [SerializeField] Rigidbody m_object;
+        private int m_step = 0;
 
         // Called once when the script starts
         void Start()
@@ -17,9 +21,20 @@ namespace Trajectory_Planner_Augmentus
         }
 
         // Called every frame
-        void Update()
+        void FixedUpdate()
         {
+            if (m_object == null || m_traj == null || m_traj.Count == 0 || m_step == m_traj.Count) return;
+
             // Update the frame
+            m_object.linearVelocity = convertPointToVec3D(m_traj.velocities[m_step]);
+            m_object.MovePosition(convertPointToVec3D(m_traj.positions[m_step]));
+            m_step++;
+        }
+
+        // For demo purpose only, assume our trajectory is in 3D Cartesian-space
+        public Vector3 convertPointToVec3D(Point point)
+        {
+            return new Vector3((float)point.dims[0], (float)point.dims[1], (float)point.dims[2]);
         }
     }
 
